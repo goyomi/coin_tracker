d0f51dd2ed7c94aa6b980820d15b1e37eac86b12
goyomi <ghdl1004797@gmail.com>
2024-03-18 23:13:30 +0900

    Feat: QueryClientProvider, ThemeProvider, ReactQueryDevtools 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={true} />
+  </QueryClientProvider>
+  // </React.StrictMode>
 );
