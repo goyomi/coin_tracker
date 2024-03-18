7c58fe1d70dee4947bf2464e528e46ac40cf3f31
goyomi <ghdl1004797@gmail.com>
2024-03-18 23:07:18 +0900

    Add, Feat: DefaultTheme interface 추가

---
+++ src/types/styled.d.ts
@@ -0,0 +1,10 @@
+declare module "theme" {
+  export interface DefaultTheme {
+    mainFontColor: string;
+    secondFontColor: string;
+    thirdFontColor: string;
+    borderColor: string;
+    changeUpColor: string;
+    changeDownColor: string;
+  }
+}
