c7dfcf3fdb1c182487cc5ad2485ba501b75c04f1
goyomi <ghdl1004797@gmail.com>
2024-03-18 23:15:53 +0900

    Design: theme 색상 정의

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  mainFontColor: "#050F19",
+  secondFontColor: "#113353",
+  thirdFontColor: "#113353",
+  borderColor: "#ECEFF1",
+  changeUpColor: "#DF5F67",
+  changeDownColor: "#3ACC8A",
+};
