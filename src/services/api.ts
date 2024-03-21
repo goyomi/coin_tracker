8b9bdf4fb0607641bb3d8b999f300e87e7b52b52
goyomi <ghdl1004797@gmail.com>
2024-03-22 00:08:52 +0900

    Feat: ohlc fetch function 추가

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,12 @@
+const BASE_URL = `https://api.coingecko.com/api/v3/coins`;
+
 export function fetchCoins() {
   return fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2"
+    `${BASE_URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2`
   ).then((response) => response.json());
+}
+
+export async function ohlc(coinId: string, days: number) {
+  console.log(coinId, days);
+  return await (await fetch(`${BASE_URL}/${coinId}/ohlc?vs_currency=usd&days=${days}`)).json();
 }
