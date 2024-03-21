ca9d039a870089bea3d6155ca68108e10ea4879d
goyomi <ghdl1004797@gmail.com>
2024-03-22 00:11:03 +0900

    Feat: Chart 컴포넌트 UI CSS 스타일링 추가

--- src/styles/chart.ts
+++ src/styles/chart.ts
@@ -1,1 +1,22 @@
-export {};
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.div`
+  margin-bottom: 1rem;
+  padding: 1rem;
+  text-align: right;
+  & > button {
+    margin-right: 1rem;
+    padding: 1rem;
+    font-size: 1.5rem;
+    border-radius: 0.5rem;
+    cursor: pointer;
+  }
+  & > button:last-child {
+    margin-right: 0;
+  }
+  .isActive {
+    font-weight: bolder;
+    background-color: ${(props) => props.theme.onActiveColor};
+    color: white;
+  }
+`;
