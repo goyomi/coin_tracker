51e5d47d9be4aab2f8ea036da78743eb4e9ef571
goyomi <ghdl1004797@gmail.com>
2024-03-22 20:30:30 +0900

    Feat: coin intro fetch function add

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,19 @@
 const BASE_URL = `https://api.coingecko.com/api/v3/coins`;
 
-export function fetchCoins() {
-  return fetch(
-    `${BASE_URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2`
-  ).then((response) => response.json());
+export async function fetchCoins() {
+  return await (
+    await fetch(
+      `${BASE_URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false&price_change_percentage=24h&locale=en&precision=2`
+    )
+  ).json();
 }
 
 export async function ohlc(coinId: string, days: number) {
   return await (
     await fetch(`${BASE_URL}/${coinId}/ohlc?vs_currency=usd&days=${days}`)
   ).json();
+}
+
+export async function introCoin(coinId: string) {
+  return await (await fetch(`${BASE_URL}/${coinId}`)).json();
 }
