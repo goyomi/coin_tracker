297bf383daa028bc56369b29df5966bc2a87e820
goyomi <ghdl1004797@gmail.com>
2024-03-22 15:21:39 +0900

    Feat: ohlc data fetch 방식 변경(실시간 차트 출력)

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -9,14 +9,41 @@ import { ButtonWrapper } from "../styles/chart";
 function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
   const [days, setDays] = useState(1);
   const [isActive, setIsActive] = useState("");
-  const { data: ohlcData, isLoading } = useQuery<IOhlc[]>(
-    ["ohlc", selectedCoin],
-    () => {
-      if (!selectedCoin?.id) return [];
-      return ohlc(selectedCoin.id, days);
-    },
-    { refetchInterval: 60000 }
-  );
+
+  // query
+  const queries = {
+    "1": useQuery<IOhlc[]>(
+      ["oneDay", "ohlc", selectedCoin],
+      () => {
+        return ohlc(selectedCoin!.id, 1);
+      },
+      { staleTime: Infinity }
+    ),
+    "7": useQuery<IOhlc[]>(
+      ["oneWeek", "ohlc", selectedCoin],
+      () => {
+        return ohlc(selectedCoin!.id, 7);
+      },
+      { staleTime: Infinity }
+    ),
+    "30": useQuery<IOhlc[]>(
+      ["oneMonth", "ohlc", selectedCoin],
+      () => {
+        return ohlc(selectedCoin!.id, 30);
+      },
+      { staleTime: Infinity }
+    ),
+    "365": useQuery<IOhlc[]>(
+      ["oneYear", "ohlc", selectedCoin],
+      () => {
+        return ohlc(selectedCoin!.id, 365);
+      },
+      { staleTime: Infinity }
+    ),
+  };
+
+  const { data: currentData, isLoading } =
+    queries[days.toString() as keyof typeof queries];
 
   // handle fn
   const handleClickBtn = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -26,8 +53,8 @@ function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
   };
 
   // chart
-  const seriesData = Array.isArray(ohlcData)
-    ? ohlcData.map((data) => {
+  const seriesData = Array.isArray(currentData)
+    ? currentData.map((data) => {
         return {
           x: new Date(data[0]),
           y: [data[1], data[2], data[3], data[4]], // [Open, High, Low, Close]
@@ -71,16 +98,32 @@ function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
   return (
     <>
       <ButtonWrapper>
-        <button className={isActive === "1" ? "isActive" : ""} id="1" onClick={handleClickBtn}>
+        <button
+          className={isActive === "1" ? "isActive" : ""}
+          id="1"
+          onClick={handleClickBtn}
+        >
           1D
         </button>
-        <button className={isActive === "7" ? "isActive" : ""} id="7" onClick={handleClickBtn}>
+        <button
+          className={isActive === "7" ? "isActive" : ""}
+          id="7"
+          onClick={handleClickBtn}
+        >
           1W
         </button>
-        <button className={isActive === "30" ? "isActive" : ""} id="30" onClick={handleClickBtn}>
+        <button
+          className={isActive === "30" ? "isActive" : ""}
+          id="30"
+          onClick={handleClickBtn}
+        >
           1M
         </button>
-        <button className={isActive === "365" ? "isActive" : ""} id="365" onClick={handleClickBtn}>
+        <button
+          className={isActive === "365" ? "isActive" : ""}
+          id="365"
+          onClick={handleClickBtn}
+        >
           1Y
         </button>
       </ButtonWrapper>
