dac525aa29f70ff5dbc96270ab55b95cb8640bd7
goyomi <ghdl1004797@gmail.com>
2024-03-22 23:59:36 +0900

    Feat: Header UI 컴포넌트 추가 및 디자인 작업, icon link 연결, email 주소 copy 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,48 @@
-export {};
+import { Link } from "react-router-dom";
+import { HeadTitle } from "../styles/header";
+
+function Header() {
+  const copyEmail = async () => {
+    try {
+      await navigator.clipboard.writeText("ghdl1004797@gamil.com");
+      alert("이메일 주소가 복사되었습니다!");
+    } catch (err) {
+      console.error("복사실패:", err);
+    }
+  };
+
+  return (
+    <HeadTitle>
+      <h1>
+        <Link to="/">
+          <img
+            className="logo"
+            src="/assets/coin_tracker_logo.jpg"
+            alt="Coin Tracker Logo"
+          />
+        </Link>
+        <div>
+          <a
+            href="https://github.com/goyomi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="github"
+              src="/assets/github_logo.png"
+              alt="GitHub Logo"
+            />
+          </a>
+          <img
+            className="email"
+            src="/assets/gmail_logo.png"
+            alt="Gmail Logo"
+            onClick={copyEmail}
+          />
+        </div>
+      </h1>
+    </HeadTitle>
+  );
+}
+
+export default Header;
