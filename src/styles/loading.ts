f133f7cf95c1379015edf1fe93655b31a7cb8624
goyomi <ghdl1004797@gmail.com>
2024-03-24 22:45:29 +0900

    Feat: Loading 컴포넌트 UI 및 스타일 작업

--- src/styles/loading.ts
+++ src/styles/loading.ts
@@ -1,1 +1,32 @@
-export {};
+import styled, { keyframes } from "styled-components";
+
+const blink = keyframes`
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+`;
+
+export const LoadingSection = styled.section`
+  height: calc(100vh - 20rem);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4rem;
+  cursor: wait;
+  img {
+    width: 50rem;
+  }
+  .loading_text_wrapper {
+    font-size: 2.5rem;
+    line-height: 3.75rem;
+    text-align: center;
+  }
+  .loading {
+    font-size: 4rem;
+    animation: ${blink} 1s infinite;
+  }
+`;
