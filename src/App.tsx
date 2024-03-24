5937439917ebbbe31a063e19742e51afecfb06fb
goyomi <ghdl1004797@gmail.com>
2024-03-24 21:09:36 +0900

    Move: Context Provider 코드 routes >> App 로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import Routes from "./routes";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { MainContainer } from "./styles/common";
+import { ICoin, IContext } from "./types/coin";
+import { fetchCoins } from "./services/api";
+import { CoinDataContext } from "./contexts/CoinDataContext";
 
 function App() {
+  const { data, isLoading } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
+
+  console.log(isLoading);
   return (
-    <>
+    <CoinDataContext.Provider value={{ data: data ?? undefined, isLoading }}>
       <GlobalStyle />
       <MainContainer>
-        <h1>Coin Tracker</h1>
         <Routes />
       </MainContainer>
-    </>
+    </CoinDataContext.Provider>
   );
 }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,19 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Coin from "./pages/Coin";
 import Coins from "./pages/Coins";
-import { CoinDataContext } from "./contexts/CoinDataContext";
-import { useQuery } from "@tanstack/react-query";
-import { ICoin } from "./types/coin";
-import { fetchCoins } from "./services/api";
 
 function Routes() {
-  const { data } = useQuery<ICoin[]>(["allCoins"], fetchCoins, { refetchInterval: 60000 });
   return (
-    <CoinDataContext.Provider value={data ?? null}>
-      <Router>
-        <Switch>
-          <Route path="/:coinId">
-            <Coin />
-          </Route>
-          <Route path="/">
-            <Coins />
-          </Route>
-        </Switch>
-      </Router>
-    </CoinDataContext.Provider>
+    <Router>
+      <Switch>
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
