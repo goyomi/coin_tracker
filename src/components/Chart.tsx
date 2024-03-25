ad393b2b353176b107486b464a9d0ed397ec788f
goyomi <ghdl1004797@gmail.com>
2024-03-25 18:43:27 +0900

    Move: Chart Query Chart.tsx >> Coin.tsx

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,48 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import { ohlc } from "../services/api";
 import ReactApexChart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
-import { ICoin, IOhlc } from "../types/coin";
+import { IChart } from "../types/coin";
 import { useState } from "react";
 import { ButtonWrapper } from "../styles/chart";
 
-function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
+function Chart({ selectedCoin, queries }: IChart) {
   const [days, setDays] = useState(1);
   const [isActive, setIsActive] = useState("1");
 
-  // query
-  const queries = {
-    "1": useQuery<IOhlc[]>(
-      ["oneDay", "ohlc", selectedCoin],
-      () => {
-        return ohlc(selectedCoin!.id, 1);
-      },
-      { staleTime: Infinity }
-    ),
-    "7": useQuery<IOhlc[]>(
-      ["oneWeek", "ohlc", selectedCoin],
-      () => {
-        return ohlc(selectedCoin!.id, 7);
-      },
-      { staleTime: Infinity }
-    ),
-    "30": useQuery<IOhlc[]>(
-      ["oneMonth", "ohlc", selectedCoin],
-      () => {
-        return ohlc(selectedCoin!.id, 30);
-      },
-      { staleTime: Infinity }
-    ),
-    "365": useQuery<IOhlc[]>(
-      ["oneYear", "ohlc", selectedCoin],
-      () => {
-        return ohlc(selectedCoin!.id, 365);
-      },
-      { staleTime: Infinity }
-    ),
-  };
-
-  const { data: currentData, isLoading } =
+  const { data: currentData } =
     queries[days.toString() as keyof typeof queries];
 
   // handle fn
@@ -52,7 +18,7 @@ function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
     setIsActive(target.id);
   };
 
-  // chart
+  // chart option
   const seriesData = Array.isArray(currentData)
     ? currentData.map((data) => {
         return {
@@ -96,40 +62,38 @@ function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
   };
 
   return (
-    <section>
-      <ButtonWrapper>
-        <button
-          className={isActive === "1" ? "isActive" : ""}
-          id="1"
-          onClick={handleClickBtn}
-        >
-          1D
-        </button>
-        <button
-          className={isActive === "7" ? "isActive" : ""}
-          id="7"
-          onClick={handleClickBtn}
-        >
-          1W
-        </button>
-        <button
-          className={isActive === "30" ? "isActive" : ""}
-          id="30"
-          onClick={handleClickBtn}
-        >
-          1M
-        </button>
-        <button
-          className={isActive === "365" ? "isActive" : ""}
-          id="365"
-          onClick={handleClickBtn}
-        >
-          1Y
-        </button>
-      </ButtonWrapper>
-      {isLoading ? (
-        <div>Loading....</div>
-      ) : (
+    <>
+      <section>
+        <ButtonWrapper>
+          <button
+            className={isActive === "1" ? "isActive" : ""}
+            id="1"
+            onClick={handleClickBtn}
+          >
+            1D
+          </button>
+          <button
+            className={isActive === "7" ? "isActive" : ""}
+            id="7"
+            onClick={handleClickBtn}
+          >
+            1W
+          </button>
+          <button
+            className={isActive === "30" ? "isActive" : ""}
+            id="30"
+            onClick={handleClickBtn}
+          >
+            1M
+          </button>
+          <button
+            className={isActive === "365" ? "isActive" : ""}
+            id="365"
+            onClick={handleClickBtn}
+          >
+            1Y
+          </button>
+        </ButtonWrapper>
         <ReactApexChart
           options={chartOptions}
           series={[{ data: seriesData || [] }]}
@@ -137,8 +101,8 @@ function Chart({ selectedCoin }: { selectedCoin: ICoin | undefined }) {
           height={350}
           width={600}
         />
-      )}
-    </section>
+      </section>
+    </>
   );
 }
 
--- src/pages/Coin.tsx
+++ src/pages/Coin.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { CoinDataContext } from "../contexts/CoinDataContext";
 import { useParams } from "react-router-dom";
 import {
@@ -16,27 +16,69 @@ import {
 } from "../styles/coin";
 import { A11y } from "../styles/common";
 import ToggleColorWithValue from "../utils/colorChangeOnValue";
-import { ICoin, ICoinIntro, IParams } from "../types/coin";
+import { ICoin, ICoinIntro, IOhlc, IParams } from "../types/coin";
 import ThousandSeparator from "../utils/thousandSeparator";
 import Chart from "../components/Chart";
 import { useQuery } from "@tanstack/react-query";
-import { introCoin } from "../services/api";
+import { introCoin, ohlc } from "../services/api";
 import parse from "html-react-parser";
+import Loading from "../components/Loading";
 
 function Coin() {
+  // data fetch - coin 상세정보
   const { data: coinData, isLoading } = useContext(CoinDataContext);
   const { coinId } = useParams<IParams>();
   const selectedCoin = coinData?.find((coin: ICoin) => coin.id === coinId);
-  const [inputOne, setInputOne] = useState<number | "">(0);
-  const [inputTwo, setInputTwo] = useState<number | "">(0);
+  // data fetch - chart
+  const query1 = useQuery<IOhlc[]>(
+    ["oneDay", "ohlc", selectedCoin],
+    () => {
+      return ohlc(selectedCoin!.id, 1);
+    },
+    { staleTime: Infinity }
+  );
+  const query7 = useQuery<IOhlc[]>(
+    ["oneWeek", "ohlc", selectedCoin],
+    () => {
+      return ohlc(selectedCoin!.id, 7);
+    },
+    { staleTime: Infinity }
+  );
+  const query30 = useQuery<IOhlc[]>(
+    ["oneMonth", "ohlc", selectedCoin],
+    () => {
+      return ohlc(selectedCoin!.id, 30);
+    },
+    { staleTime: Infinity }
+  );
+  const query365 = useQuery<IOhlc[]>(
+    ["oneYear", "ohlc", selectedCoin],
+    () => {
+      return ohlc(selectedCoin!.id, 365);
+    },
+    { staleTime: Infinity }
+  );
 
-  const { data: coinIntro } = useQuery<ICoinIntro>(
+  const queries = useMemo(
+    () => ({
+      "1": query1,
+      "7": query7,
+      "30": query30,
+      "365": query365,
+    }),
+    [query1, query7, query30, query365]
+  );
+  // data fetch - coin 소개(about)
+  const { data: coinIntro, isLoading: coinIntroLoading } = useQuery<ICoinIntro>(
     ["intro", coinId],
     () => introCoin(coinId),
     { staleTime: Infinity }
   );
-
   const textLine = coinIntro?.description.en.split("\r\n");
+
+  const [inputOne, setInputOne] = useState<number | "">(0);
+  const [inputTwo, setInputTwo] = useState<number | "">(0);
+  const [chartQueryLoading, setChartQueryLoading] = useState(false);
 
   const handleInputOneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = Number(event.target.value);
@@ -60,146 +102,166 @@ function Coin() {
     setInputTwo("");
   };
 
+  useEffect(() => {
+    const isQueriesLoading = Object.values(queries).some(
+      (query) => query.isLoading
+    );
+    setChartQueryLoading(isQueriesLoading);
+  }, [queries]);
+
+  const isLoadingState = [isLoading, chartQueryLoading, coinIntroLoading];
+  const isAnyLoading = isLoadingState.some((state) => state);
+
   return (
-    <CoinWrapper>
-      <A11y>Coin detail page</A11y>
-      <LeftZone>
-        {selectedCoin ? (
-          <CoinData key={selectedCoin.id}>
-            <CoinTitle>
-              <img src={selectedCoin.image} alt={selectedCoin.name} />
-              <span className="coin_name">{selectedCoin.name}</span>
-              <span className="coin_symbol">{selectedCoin.symbol}</span>
-              <span className="coin_rank">
-                # {selectedCoin.market_cap_rank}
-              </span>
-            </CoinTitle>
-            <CoinPrice>
-              <span>US $</span>
-              <ThousandSeparator number={selectedCoin.current_price} />
-              <ToggleColorWithValue
-                number={selectedCoin.price_change_percentage_24h}
-              />
-            </CoinPrice>
-            <CoinDataTable>
-              <tbody>
-                <tr>
-                  <th>Market Cap</th>
-                  <td>
-                    <ThousandSeparator number={selectedCoin.market_cap} />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Fully Diluted Valuation</th>
-                  <td>
-                    <ThousandSeparator
-                      number={selectedCoin.fully_diluted_valuation}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>24 Hour Trading Vol</th>
-                  <td>
-                    <ThousandSeparator number={selectedCoin.total_volume} />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Circulating Supply</th>
-                  <td>
-                    <ThousandSeparator
-                      number={selectedCoin.circulating_supply}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Total Supply</th>
-                  <td>
-                    <ThousandSeparator number={selectedCoin.total_supply} />
-                  </td>
-                </tr>
-                <tr>
-                  <th>Max Supply</th>
-                  <td>
-                    <ThousandSeparator number={selectedCoin.max_supply} />
-                  </td>
-                </tr>
-              </tbody>
-            </CoinDataTable>
-          </CoinData>
-        ) : null}
-        <CoinConvertor>
-          <h4>{selectedCoin?.symbol} Converter</h4>
-          <div className="input_wrapper">
-            <input
-              type="number"
-              value={inputOne}
-              onChange={handleInputOneChange}
-              onFocus={handleInputOneFocus}
-            />
-            <span>{selectedCoin?.symbol}</span>
-          </div>
-          <div className="input_wrapper">
-            <input
-              type="number"
-              value={inputTwo}
-              onChange={handleInputTwoChange}
-              onFocus={handleInputTwoFocus}
-            />
-            <span>USD</span>
-          </div>
-        </CoinConvertor>
-        <HistoricalPrice>
-          <h2>{selectedCoin?.symbol} Historical Price</h2>
-          <HistoricalPriceTable>
-            <tbody>
-              <tr>
-                <th>24h Range</th>
-                <td>
-                  <ThousandSeparator number={selectedCoin?.low_24h} />
-                  <span className="separator">-</span>
-                  <ThousandSeparator number={selectedCoin?.high_24h} />
-                </td>
-              </tr>
-              <tr>
-                <th>All-Time High</th>
-                <td>
-                  <i>
-                    (
-                    {selectedCoin?.ath_date
-                      ? new Date(selectedCoin?.ath_date).toLocaleDateString()
-                      : ""}
-                    )
-                  </i>
-                  <ThousandSeparator number={selectedCoin?.ath} />
-                </td>
-              </tr>
-              <tr>
-                <th>All-Time Low</th>
-                <td>
-                  <i>
-                    (
-                    {selectedCoin?.atl_date
-                      ? new Date(selectedCoin.atl_date).toLocaleDateString()
-                      : ""}
-                    )
-                  </i>
-                  <ThousandSeparator number={selectedCoin?.atl} />
-                </td>
-              </tr>
-            </tbody>
-          </HistoricalPriceTable>
-        </HistoricalPrice>
-      </LeftZone>
-      <RightZone>
-        <Chart selectedCoin={selectedCoin} />
-        <CoinIntro>
-          <h2>About</h2>
-          {textLine?.map((text: string, idx: number) => (
-            <React.Fragment key={idx}>{parse(`<p>${text}</p>`)}</React.Fragment>
-          ))}
-        </CoinIntro>
-      </RightZone>
-    </CoinWrapper>
+    <>
+      {isAnyLoading ? (
+        <Loading />
+      ) : (
+        <CoinWrapper>
+          <A11y as="h1">Coin detail page</A11y>
+          <LeftZone>
+            {selectedCoin ? (
+              <CoinData key={selectedCoin.id}>
+                <CoinTitle>
+                  <img src={selectedCoin.image} alt={selectedCoin.name} />
+                  <span className="coin_name">{selectedCoin.name}</span>
+                  <span className="coin_symbol">{selectedCoin.symbol}</span>
+                  <span className="coin_rank">
+                    # {selectedCoin.market_cap_rank}
+                  </span>
+                </CoinTitle>
+                <CoinPrice>
+                  <span>US $</span>
+                  <ThousandSeparator number={selectedCoin.current_price} />
+                  <ToggleColorWithValue
+                    number={selectedCoin.price_change_percentage_24h}
+                  />
+                </CoinPrice>
+                <CoinDataTable>
+                  <tbody>
+                    <tr>
+                      <th>Market Cap</th>
+                      <td>
+                        <ThousandSeparator number={selectedCoin.market_cap} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>Fully Diluted Valuation</th>
+                      <td>
+                        <ThousandSeparator
+                          number={selectedCoin.fully_diluted_valuation}
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>24 Hour Trading Vol</th>
+                      <td>
+                        <ThousandSeparator number={selectedCoin.total_volume} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>Circulating Supply</th>
+                      <td>
+                        <ThousandSeparator
+                          number={selectedCoin.circulating_supply}
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>Total Supply</th>
+                      <td>
+                        <ThousandSeparator number={selectedCoin.total_supply} />
+                      </td>
+                    </tr>
+                    <tr>
+                      <th>Max Supply</th>
+                      <td>
+                        <ThousandSeparator number={selectedCoin.max_supply} />
+                      </td>
+                    </tr>
+                  </tbody>
+                </CoinDataTable>
+              </CoinData>
+            ) : null}
+            <CoinConvertor>
+              <h4>{selectedCoin?.symbol} Converter</h4>
+              <div className="input_wrapper">
+                <input
+                  type="number"
+                  value={inputOne}
+                  onChange={handleInputOneChange}
+                  onFocus={handleInputOneFocus}
+                />
+                <span>{selectedCoin?.symbol}</span>
+              </div>
+              <div className="input_wrapper">
+                <input
+                  type="number"
+                  value={inputTwo}
+                  onChange={handleInputTwoChange}
+                  onFocus={handleInputTwoFocus}
+                />
+                <span>USD</span>
+              </div>
+            </CoinConvertor>
+            <HistoricalPrice>
+              <h2>{selectedCoin?.symbol} Historical Price</h2>
+              <HistoricalPriceTable>
+                <tbody>
+                  <tr>
+                    <th>24h Range</th>
+                    <td>
+                      <ThousandSeparator number={selectedCoin?.low_24h} />
+                      <span className="separator">-</span>
+                      <ThousandSeparator number={selectedCoin?.high_24h} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>All-Time High</th>
+                    <td>
+                      <i>
+                        (
+                        {selectedCoin?.ath_date
+                          ? new Date(
+                              selectedCoin?.ath_date
+                            ).toLocaleDateString()
+                          : ""}
+                        )
+                      </i>
+                      <ThousandSeparator number={selectedCoin?.ath} />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>All-Time Low</th>
+                    <td>
+                      <i>
+                        (
+                        {selectedCoin?.atl_date
+                          ? new Date(selectedCoin.atl_date).toLocaleDateString()
+                          : ""}
+                        )
+                      </i>
+                      <ThousandSeparator number={selectedCoin?.atl} />
+                    </td>
+                  </tr>
+                </tbody>
+              </HistoricalPriceTable>
+            </HistoricalPrice>
+          </LeftZone>
+          <RightZone>
+            <Chart selectedCoin={selectedCoin} queries={queries} />
+            <CoinIntro>
+              <h2>About</h2>
+              {textLine?.map((text: string, idx: number) => (
+                <React.Fragment key={idx}>
+                  {parse(`<p>${text}</p>`)}
+                </React.Fragment>
+              ))}
+            </CoinIntro>
+          </RightZone>
+        </CoinWrapper>
+      )}
+    </>
   );
 }
 
