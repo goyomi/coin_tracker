f3993f0dddbb3383f17481979cbba42b525531c1
goyomi <ghdl1004797@gmail.com>
2024-03-25 23:15:54 +0900

    Remove: console 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import Routes from "./routes";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { MainContainer } from "./styles/common";
-import { ICoin, IContext } from "./types/coin";
+import { ICoin } from "./types/coin";
 import { fetchCoins } from "./services/api";
 import { CoinDataContext } from "./contexts/CoinDataContext";
 
 function App() {
   const { data, isLoading } = useQuery<ICoin[]>(["allCoins"], fetchCoins);
 
-  console.log(isLoading);
   return (
     <CoinDataContext.Provider value={{ data: data ?? undefined, isLoading }}>
       <GlobalStyle />
