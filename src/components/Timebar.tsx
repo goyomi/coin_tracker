84ed68051a76ffe4947ac1f3759fac86f26bfd6b
goyomi <ghdl1004797@gmail.com>
2024-03-28 15:56:26 +0900

    Move, Remove, Fix: Timebar style component 코드 이동 및 파일 삭제, Timebar key warning 해결

--- src/components/Timebar.tsx
+++ src/components/Timebar.tsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
-import { ButtonWrapper } from "../styles/timebar";
 import { ITimes } from "../types/coin";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 1rem;
+  padding: 1rem;
+  text-align: right;
+  ol {
+    display: flex;
+    gap: 1rem;
+  }
+  ol li button {
+    padding: 1rem;
+    font-size: 1.5rem;
+    border-radius: 0.5rem;
+    cursor: pointer;
+  }
+  .isActive {
+    font-weight: bolder;
+    background-color: ${(props) => props.theme.onActiveColor};
+    color: white;
+  }
+`;
 
 function Timebar({ times, setDays, setTime }: ITimes) {
   const [isActive, setIsActive] = useState(Object.keys(times)[0]);
@@ -14,11 +35,15 @@ function Timebar({ times, setDays, setTime }: ITimes) {
 
   return (
     <ButtonWrapper>
-      {Object.entries(times).map(([id, value]) => (
-        <button id={id} className={isActive === id ? "isActive" : ""} onClick={handleClickBtn}>
-          {value}
-        </button>
-      ))}
+      <ol>
+        {Object.entries(times).map(([id, value]) => (
+          <li key={id} id={id}>
+            <button id={id} className={isActive === id ? "isActive" : ""} onClick={handleClickBtn}>
+              {value}
+            </button>
+          </li>
+        ))}
+      </ol>
     </ButtonWrapper>
   );
 }
