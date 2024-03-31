b5c2788d4b141bf70edd099dc2a5bcbc15551647
goyomi <ghdl1004797@gmail.com>
2024-03-31 22:29:05 +0900

    Move, Feat: ThemeProvider index에서 App으로 코드 이동, ToggleOn 상태 코드 이동 및 props로 내리기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { MainContainer } from "./styles/common";
-import { ICoin } from "./types/coin";
+import { ICoin } from "./types/type";
 import { fetchCoins } from "./services/api";
 import { CoinDataContext } from "./contexts/CoinDataContext";
 import { useState } from "react";
@@ -9,37 +9,38 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Coin from "./pages/Coin";
 import Coins from "./pages/Coins";
 import ErrorPage from "./pages/ErrorPage";
-import Loading from "./pages/LoadingPage";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, theme } from "./styles/theme";
 
 function App() {
   const [time, setTime] = useState("1h");
+  const [toggleOn, setToggleOn] = useState(false);
   const { data, isLoading, isError, error } = useQuery<ICoin[]>(["allCoins", time], () => fetchCoins(time), {
     staleTime: Infinity,
   });
 
   return (
     <CoinDataContext.Provider value={{ data, isLoading, isError, error }}>
-      <GlobalStyle />
-      <BrowserRouter basename="/coin_tracker">
-        <Switch>
-          <Route path="/loading">
-            <Loading />
-          </Route>
-          <Route path="/error">
-            <ErrorPage />
-          </Route>
-          <Route path="/:coinId">
-            <MainContainer>
-              <Coin />
-            </MainContainer>
-          </Route>
-          <Route path="/">
-            <MainContainer>
-              <Coins time={time} setTime={setTime} />
-            </MainContainer>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={toggleOn ? darkTheme : theme}>
+        <GlobalStyle />
+        <BrowserRouter basename="/coin_tracker">
+          <Switch>
+            <Route path="/error">
+              <ErrorPage />
+            </Route>
+            <Route path="/:coinId">
+              <MainContainer>
+                <Coin toggleOn={toggleOn} setToggleOn={setToggleOn} />
+              </MainContainer>
+            </Route>
+            <Route path="/">
+              <MainContainer>
+                <Coins time={time} setTime={setTime} toggleOn={toggleOn} setToggleOn={setToggleOn} />
+              </MainContainer>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </CoinDataContext.Provider>
   );
 }
