56869110facf82e6b53fe5423f1f6db4f0814668
goyomi <ghdl1004797@gmail.com>
2024-04-11 14:43:09 +0900

    Refactor: 중복요소 제거, 스타일컴포넌트 분리작업

--- src/components/CoinConverter.tsx
+++ src/components/CoinConverter.tsx
@@ -3,77 +3,103 @@ import styled from "styled-components";
 import { ICoin } from "../types/type";
 import { ScreenReaderOnly } from "../styles/common";
 import { CURRENCY } from "../constant";
+import { Heading } from "../styles/coin";
 
 const Section = styled.section`
   font-size: 2rem;
 `;
 
-const Heading = styled.h3`
-  padding: 2rem 0;
-  span:first-child {
-    margin-right: 1rem;
-    text-transform: uppercase;
-  }
-  span:nth-child(2) {
-    text-transform: capitalize;
-  }
-`;
-
-const InputWrapper = styled.div`
+const InputWrapper = styled.div<{ marginBottom: boolean }>`
   border: 0.15rem solid ${(props) => props.theme.grey1Color};
   border-radius: 0.8rem;
   padding: 1.2rem 2rem;
-  input {
-    width: 80%;
-    font-size: 1.8rem;
-    border: none;
-    border-radius: 0.8rem;
-    color: ${(props) => props.theme.mainFontColor};
-    background-color: ${(props) => props.theme.backgroundColor};
-  }
-  span {
-    width: 20%;
-    display: inline-block;
-    margin-left: auto;
-    font-size: 1.8rem;
-    text-align: right;
-    text-transform: uppercase;
-    color: grey;
-  }
+  margin-bottom: ${(props) => (props.marginBottom ? "1rem" : "")};
 `;
 
-const CoinInputWrapper = styled(InputWrapper)`
-  margin-bottom: 1rem;
+const InputInner = styled.input`
+  width: 80%;
+  font-size: 1.8rem;
+  border: none;
+  border-radius: 0.8rem;
+  color: ${(props) => props.theme.mainFontColor};
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Unit = styled.span`
+  width: 20%;
+  display: inline-block;
+  margin-left: auto;
+  font-size: 1.8rem;
+  text-align: right;
+  text-transform: uppercase;
+  color: grey;
 `;
 
 interface ICoinConverterProps {
   selectedCoin: ICoin;
 }
 
+interface IInputValueState {
+  coinAmount: number | "";
+  currencyAmount: number | "";
+}
+
 function CoinConverter({ selectedCoin }: ICoinConverterProps) {
-  const [inputOne, setInputOne] = useState<number | "">(0);
-  const [inputTwo, setInputTwo] = useState<number | "">(0);
+  const [inputValue, setInputValue] = useState<IInputValueState>({
+    coinAmount: 0,
+    currencyAmount: 0,
+  });
   const price = selectedCoin?.current_price || 1;
 
-  const handleInputOneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Number(event.target.value);
-    setInputOne(value);
-    setInputTwo(value * price);
+  const COIN_AMOUNT = "coinAmount";
+  const CURRENCY_AMOUNT = "currencyAmount";
+  const handleInputChange = (name: string, value: number | string) => {
+    const numberValue = typeof value === "number" ? value : Number(value);
+    name === COIN_AMOUNT
+      ? setInputValue(() => ({ coinAmount: numberValue, currencyAmount: numberValue * price }))
+      : setInputValue(() => ({ coinAmount: numberValue / price, currencyAmount: numberValue }));
   };
 
-  const handleInputTwoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = Number(event.target.value);
-    setInputTwo(value);
-    setInputOne(value / price);
+  const handleInputFocus = (name: string) => {
+    name === COIN_AMOUNT
+      ? setInputValue((prev) => ({ ...prev, coinAmount: "" }))
+      : setInputValue((prev) => ({ ...prev, currencyAmount: "" }));
   };
 
-  const handleInputOneFocus = () => {
-    setInputOne("");
-  };
-
-  const handleInputTwoFocus = () => {
-    setInputTwo("");
-  };
+  const data = [
+    {
+      title: selectedCoin?.symbol,
+      value: selectedCoin?.symbol,
+      input: (
+        <InputInner
+          id={COIN_AMOUNT}
+          type="number"
+          value={inputValue.coinAmount}
+          onChange={(event) => {
+            const value = Number(event.target.value);
+            handleInputChange(COIN_AMOUNT, value);
+          }}
+          onFocus={() => handleInputFocus(COIN_AMOUNT)}
+        />
+      ),
+    },
+    {
+      title: CURRENCY,
+      value: CURRENCY,
+      input: (
+        <InputInner
+          id={CURRENCY_AMOUNT}
+          type="number"
+          value={inputValue.currencyAmount}
+          onChange={(event) => {
+            const value = Number(event.target.value);
+            handleInputChange(CURRENCY_AMOUNT, value);
+          }}
+          onFocus={() => handleInputFocus(CURRENCY_AMOUNT)}
+        />
+      ),
+    },
+  ];
 
   return (
     <Section>
@@ -81,20 +107,15 @@ function CoinConverter({ selectedCoin }: ICoinConverterProps) {
         <span>{selectedCoin?.symbol}</span>
         <span>Converter</span>
       </Heading>
-      <CoinInputWrapper>
-        <ScreenReaderOnly as="label" htmlFor="inputOne">
-          {selectedCoin?.symbol} Amount
-        </ScreenReaderOnly>
-        <input type="number" value={inputOne} onChange={handleInputOneChange} onFocus={handleInputOneFocus} />
-        <span>{selectedCoin?.symbol}</span>
-      </CoinInputWrapper>
-      <InputWrapper>
-        <ScreenReaderOnly as="label" htmlFor="inputOne">
-          {CURRENCY} Amount
-        </ScreenReaderOnly>
-        <input type="number" value={inputTwo} onChange={handleInputTwoChange} onFocus={handleInputTwoFocus} />
-        <span>{CURRENCY}</span>
-      </InputWrapper>
+      {data.map(({ title, value, input }, index) => (
+        <InputWrapper key={index} marginBottom={true}>
+          <ScreenReaderOnly as="label" htmlFor={title === selectedCoin?.symbol ? COIN_AMOUNT : CURRENCY_AMOUNT}>
+            {title} Amount
+          </ScreenReaderOnly>
+          {input}
+          <Unit>{value}</Unit>
+        </InputWrapper>
+      ))}
     </Section>
   );
 }
