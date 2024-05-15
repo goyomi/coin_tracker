a73d8e9867a404ec1cf5c9ce2a7e77253034d974
goyomi <ghdl1004797@gmail.com>
2024-05-15 23:32:49 +0900

    Refactor: URL 쿼리 파라미터 정리

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,21 +1,35 @@
+import createUrl from "../utils/createUrl";
+
 const BASE_URL = `https://api.coingecko.com/api/v3/coins`;
 
 export async function fetchCoins() {
-  return await (
-    await fetch(
-      `${BASE_URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en`
-    )
-  ).json();
+  const url = createUrl(BASE_URL, "markets", {
+    vs_currency: "usd",
+    order: "market_cap_desc",
+    per_page: "100",
+    page: "1",
+    sparkline: "false",
+    price_change_percentage: "24h",
+    locale: "en",
+  });
+  return await (await fetch(url)).json();
 }
 
 export async function ohlc(coinId: string, days: number) {
-  return await (await fetch(`${BASE_URL}/${coinId}/ohlc?vs_currency=usd&days=${days}`)).json();
+  const url = createUrl(BASE_URL, `${coinId}/ohlc`, {
+    vs_currency: "usd",
+    days: `${days}`,
+  });
+  return await (await fetch(url)).json();
 }
 
 export async function introCoin(coinId: string) {
-  return await (
-    await fetch(
-      `${BASE_URL}/${coinId}?tickers=false&market_data=false&community_data=false&developer_data=false&sparkline=false`
-    )
-  ).json();
+  const url = createUrl(BASE_URL, `${coinId}`, {
+    tickers: "false",
+    market_data: "false",
+    community_data: "false",
+    developer_data: "false",
+    sparkline: "false",
+  });
+  return await (await fetch(url)).json();
 }
